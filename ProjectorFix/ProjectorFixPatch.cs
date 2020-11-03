b2d73b66716837ef7ceeb3835d1fead4a4629441
despc <idespc@gmail.com>
2020-11-03 16:24:10 +0300

    Initial commit

---
+++ ProjectorFix/ProjectorFixPatch.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using NLog;
+using Sandbox.Game.Entities.Blocks;
+using Torch.Managers.PatchManager;
+using VRage.Game;
+
+namespace ProjectorFix
+{
+    [PatchShim]
+    public static class ProjectorFixPatch
+    {
+        private static readonly MethodInfo NewBlueprintMethod =
+            typeof(MyProjectorBase).GetMethod("OnNewBlueprintSuccess", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public static void Patch(PatchContext ctx)
+        {
+            Log.Info("Patch init");
+            ctx.GetPattern(typeof(MyProjectorBase).GetMethod("OnNewBlueprintSuccess",
+                BindingFlags.Instance | BindingFlags.NonPublic)).Prefixes.Add(
+                typeof(ProjectorFixPatch).GetMethod(nameof(PrefixNewBlueprint),
+                    BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
+        }
+
+
+        private static void PrefixNewBlueprint(MyProjectorBase __instance,
+            ref List<MyObjectBuilder_CubeGrid> projectedGrids)
+        {
+            var proj = __instance;
+            if (proj == null)
+            {
+                Log.Warn("No projector?");
+            }
+
+            var grid = projectedGrids[0];
+
+            var projectedBlocks = grid.CubeBlocks;
+
+            foreach (var block in projectedBlocks)
+            {
+                if (block.ConstructionStockpile == null || block.ConstructionStockpile.Items.Length == 0)
+                {
+                    continue;
+                }
+
+                block.ConstructionStockpile = null;
+            }
+        }
+    }
+}
---
+++ ProjectorFix/ProjectorFixPlugin.cs
@@ -0,0 +1,16 @@
+﻿using NLog;
+using Torch;
+using Torch.API;
+
+namespace ProjectorFix
+{
+    public class ProjectorFixPlugin : TorchPluginBase
+    {
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public override void Init(ITorchBase torch)
+        {
+            Log.Info("Init ProjectorFixPlugin");
+        }
+    }
+}
